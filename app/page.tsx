82555b416e66c607cc3f9b1cff10069604cbe9b9
Tarek Molla <tarekmolla228@gmail.com>
2026-06-01 22:18:51 +0600

    Fix: clear package dependencies and route relative imports directly

--- app/page.tsx
+++ app/page.tsx
@@ -80,8 +80,8 @@ export default function HomePage() {
                   >
                     <path clipRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fillRule="evenodd" />
                   </svg>
-                  {/* Sliding Metallic Glare Screen */}
-                  <span className="absolute w-[100px] h-full bg-gradient-to-r from-transparent via-white/40 to-transparent top-0 left-[-100px] opacity-60 group-hover:animate-[shine_1.5s_ease-out_infinite]" />
+                  {/* Pure Tailwind Animated Glare Effect */}
+                  <span className="absolute w-[100px] h-full bg-gradient-to-r from-transparent via-white/30 to-transparent top-0 left-[-100px] opacity-60 group-hover:animate-[shine_1.5s_ease-out_infinite]" />
                 </button>
               </Link>
 
